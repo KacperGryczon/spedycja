7254ec870e5e8beba5accd70f8f0158c427af901
KacperGryczon <kacper.gryczon@gmail.com>
2025-11-10 17:02:42 +0100

    Test

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const nav = document.querySelector("nav");
 const heroAnimation = document.getElementById("heroAnimationId");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    nav.classList.add("nav-scrolled");
-  }
-  if (window.scrollY < 50) {
-    nav.classList.remove("nav-scrolled");
-  }
-});
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 50) {
+//     nav.classList.add("nav-scrolled");
+//   }
+//   if (window.scrollY < 50) {
+//     nav.classList.remove("nav-scrolled");
+//   }
+// });
 
 const openSidebar = document.querySelector(".bars");
 const closeSidebar = document.querySelector(".x");
